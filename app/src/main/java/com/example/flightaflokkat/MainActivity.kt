622f0711a4b879a6f3fce0a75b8419d6c3644aaf
sergio <miranda@goodbarber.com>
2021-03-02 10:17:55 +0100

    spinner display OK

--- app/src/main/java/com/example/flightaflokkat/MainActivity.kt
+++ app/src/main/java/com/example/flightaflokkat/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.flightaflokkat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.flightstats.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Récupérer aiportlist
+        val airportList = Utils.generateAirportList()
+
+        //Générer list de noms d'aeroport
+        val airportListName = ArrayList<String>()
+        for (airport in airportList) {
+            airportListName.add(airport.getFormattedName())
+        }
+
+        //Générer arrayadapter
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, airportListName)
+
+        //Spinner adapter = arrayadapter
+        val spinner = findViewById<Spinner>(R.id.airportSpinner)
+        spinner.adapter = arrayAdapter
+
+
     }
 }
